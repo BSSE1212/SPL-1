9bb9ed5d1f3788e66c12a33fa53baa2be126e994
BSSE1212 <77398804+BSSE1212@users.noreply.github.com>
2022-03-23 23:53:34 +0600

    encryption uncomplete

--- Login function.c
+++ Login function.c
@@ -26,6 +26,7 @@ if(getch()==13)
       char c,name[30],pass[30]; int z=0;
       int checku,checkp;
       fp=fopen("Web_reg.txt","rb");
+
       printf("\n\n\t\t\t\tWELCOME TO LOG IN ZONE");
       printf("\n\t\t\t\t^^^^^^^^^^^^^^^^^^^^^^");
       for(i=0;i<1000;i++)
@@ -40,25 +41,26 @@ if(getch()==13)
         }
         pass[z]='\0';
       while(!feof(fp))
-        {
+      {
         fread(&w[i],sizeof(w[i]),1,fp);
-          checku=strcmp(name,w[i].name);
-          checkp=strcmp(pass,w[i].pass);
+        checku=strcmp(name,w[i].name);
+        checkp=strcmp(pass,w[i].pass);
+
           if(checku==0&&checkp==0)
           {
-            clrscr();
-            printf("\n\n\n\t\t\tYOU HAVE LOGGED IN SUCCESSFULLY!!");
-            printf("\n\n\n\t\t\t\tWELCOME, HAVE A NICE DAY");
-            break;
+           clrscr();
+           printf("\n\n\n\t\t\tYOU HAVE LOGGED IN SUCCESSFULLY!!");
+           printf("\n\n\n\t\t\t\tWELCOME, HAVE A NICE DAY");
+           break;
           }
-        else if(checku==0&&checkp!=0)
+          else if(checku==0&&checkp!=0)
           {
             printf("\n\n\n\t\t\tWRONG PASSWORD!! Not %s??",name);
             printf("\n\n\t\t\t\t  (Press 'Y' to re-login)");
             if(getch()=='y'||getch()=='Y')
               login();
           }
-        else if(checku!=0)
+         else if(checku!=0)
           {
             printf("\n\n\n\t\t\tYou are not a Registered User\n \t\t\tPress enter to register yourself");
             if(getch()==13)
